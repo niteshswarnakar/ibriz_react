9ac66140bf25a79359c101718ef57d5b572aa203
iamnitesh <nitesh.swarnakar1@gmail.com>
2022-06-11 23:28:30 +0545

    responsive css

--- src/Header.js
+++ src/Header.js
@@ -14,37 +14,39 @@ function Header() {
   };
 
   return (
-    <div className="header">
-      <div className="header__left">
-        <p className="portfo">
-          ibriz<span className="test"> test.</span>
-        </p>
-      </div>
-      <div className="header__nav">
-        <Link className="nav__link n1" to="/">
-          Home
-        </Link>
-        <Link className="nav__link n2" to="/blogs">
-          Top Blogs
-        </Link>
-        {!isLoggedIn ? (
-          <Link className="nav__link n2" to="/login">
-            <Button type="button">Login</Button>
+    <div className="my-header">
+      <div className="header">
+        <div className="header__left">
+          <p className="portfo">
+            ibriz<span className="test"> test.</span>
+          </p>
+        </div>
+        <div className="header__nav">
+          <Link className="nav__link n1" to="/">
+            Home
           </Link>
-        ) : (
-          <Link className="nav__link n2" to="/">
-            <Button onClick={onLogout} type="button">
-              Logout
-            </Button>
+          <Link className="nav__link n2" to="/blogs">
+            Top Blogs
           </Link>
-        )}
-        {!isLoggedIn ? (
-          <Link className="nav__link n2" to="/signup">
-            <Button type="button">Sign up</Button>
-          </Link>
-        ) : (
-          ""
-        )}
+          {!isLoggedIn ? (
+            <Link className="nav__link n2" to="/login">
+              <Button type="button">Login</Button>
+            </Link>
+          ) : (
+            <Link className="nav__link n2" to="/">
+              <Button onClick={onLogout} type="button">
+                Logout
+              </Button>
+            </Link>
+          )}
+          {!isLoggedIn ? (
+            <Link className="nav__link n2" to="/signup">
+              <Button type="button">Sign up</Button>
+            </Link>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
     </div>
   );
